5ef2c16c19379309ac54246e62cb5c8107f930db
DocDrydenn <48564375+DocDrydenn@users.noreply.github.com>
2022-01-24 20:18:25 -0500

    Delete xmrig directory

--- xmrig/start-example.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-screen -wipe
-screen -dm /root/xmrig-build/xmrig/xmrig -o <pool_IP>:<pool_port> -l /var/log/xmrig-cpu.log --donate-level 1 --rig-id <rig_name> -k --verbose
-screen -r
